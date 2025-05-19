2b52d6f396694568f5580e95ca3a8d2102f4909b
unknown <dmshastel@gmail.com>
2025-05-19 17:08:00 +0300

    root layout styles are added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,24 @@
 import React from "react";
+import NextTopLoader from 'nextjs-toploader'
+import {Theme} from "@radix-ui/themes";
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+import '@/styles/_index.scss'
+
+import styles from './layout.module.scss'
+
+export default function RootLayout({children}: { children: React.ReactNode }) {
     return (
         <html lang="en">
-        <body>{children}</body>
+        <body>
+        <NextTopLoader color={'#397df6'}>
+            {/*добавить компонент Providers*/}
+            {/*<Providers>*/}
+            <Theme className={styles.bodyContainer}>
+                <h1>Header</h1>
+            </Theme>
+            {/*</Providers>*/}
+        </NextTopLoader>
+        </body>
         </html>
     );
 }
